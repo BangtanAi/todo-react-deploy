b95d6e923e5f26da4d5d6007c7adcf8327cd109c
BangtanAi <kairova-94@mail.ru>
2021-05-10 16:23:53 +0600

    Updates

---
+++ precache-manifest.9d1c766f2f41ad66a1ed665e7b16324d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b326900a7fc968156136106101dba9b3",
+    "url": "/todo-react-deploy/index.html"
+  },
+  {
+    "revision": "4dc924f63546c76939d0",
+    "url": "/todo-react-deploy/static/css/main.2b80aed3.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react-deploy/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react-deploy/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc924f63546c76939d0",
+    "url": "/todo-react-deploy/static/js/main.9bf58c49.chunk.js"
+  },
+  {
+    "revision": "41d8374782710135c7a4",
+    "url": "/todo-react-deploy/static/js/runtime-main.98093a87.js"
+  }
+]);
